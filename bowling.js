ca5131edc4dd7fddb135e7f5c8316aa699788f8b
Bas Bossink <bas.bossink@gmail.com>
2014-02-09 21:23:30 +0100

    Refactored use of score to persisting the rolls in an array.

--- bowling.js
+++ bowling.js
@@ -18,12 +18,18 @@ along with javascript-kata.  If not, see <http://www.gnu.org/licenses/>.
   'use strict';
 
   exports.newGame = function() {
-    var score = 0;
+    var rolls = [];
     return {
       roll: function(pins) {
-        score += pins;
+        rolls.push(pins);
       },
-      score: function() { return score; }
+      score: function() {
+        var score = 0, rollIndex = 0;
+        for(; rollIndex < 20; rollIndex += 1) {
+          score += rolls[rollIndex];
+        }
+        return score;
+      }
     };
   };
 }());
