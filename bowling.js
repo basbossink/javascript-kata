8a7d7b493a5bb2551d21025f7f07e6322dc17318
Bas Bossink <bas.bossink@gmail.com>
2014-02-09 21:07:19 +0100

    First passing test.

--- bowling.js
+++ bowling.js
@@ -18,6 +18,9 @@ along with javascript-kata.  If not, see <http://www.gnu.org/licenses/>.
   'use strict';
 
   exports.newGame = function() {
-
+    return {
+      roll: function() {},
+      score: function() { return 0; }
+    };
   };
 }());
