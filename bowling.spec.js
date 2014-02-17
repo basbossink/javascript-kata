ef129110ba5dc3e1f74dc81553b435fd822980af
Bas Bossink <bas.bossink@gmail.com>
2014-02-17 20:14:08 +0100

    Disable test to commence refactoring.

--- bowling.spec.js
+++ bowling.spec.js
@@ -37,5 +37,12 @@ along with javascript-kata.  If not, see <http://www.gnu.org/licenses/>.
       rollMany(1, 20);
       expect(game.score()).toBe(20);
     });
+    xit('a spare should result in the next ball being counted as a bonus', function() {
+      game.roll(4);
+      game.roll(6);
+      game.roll(3);
+      rollMany(0, 17);
+      expect(game.score()).toBe(16);
+    });
   });
 }());
