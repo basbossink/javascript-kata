713eb70eb67a6b8eb8f507f7c105c4f3c81dcee8
Bas Bossink <bas.bossink@gmail.com>
2014-02-17 20:34:52 +0100

    Add spare test case.

--- bowling.js
+++ bowling.js
@@ -26,7 +26,12 @@ along with javascript-kata.  If not, see <http://www.gnu.org/licenses/>.
       score: function() {
         var score = 0, rollIndex = 0;
         for(; rollIndex < rolls.length; rollIndex += 1) {
-          score += rolls[rollIndex];
+          if(rolls[rollIndex] + rolls[rollIndex + 1] === 10) {
+            score += 10 + rolls[rollIndex + 2];
+            rollIndex += 1;
+          } else {
+            score += rolls[rollIndex];
+          }
         }
         return score;
       }
--- bowling.spec.js
+++ bowling.spec.js
@@ -37,7 +37,7 @@ along with javascript-kata.  If not, see <http://www.gnu.org/licenses/>.
       rollMany(1, 20);
       expect(game.score()).toBe(20);
     });
-    xit('a spare should result in the next ball being counted as a bonus', function() {
+    it('a spare should result in the next ball being counted as a bonus', function() {
       game.roll(4);
       game.roll(6);
       game.roll(3);
